d14dbf717af47d2696434a6d6e044ba262398a90
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-02-23 12:50:02 +1000

    feat: random working

--- backend/src/db.py
+++ backend/src/db.py
@@ -6,56 +6,7 @@ from dotenv import load_dotenv
 from models.index import Feedback
 from models.index import Ordinal
 
-from pydantic import BaseModel
-from pydantic_mongo import AbstractRepository
-
 load_dotenv()
-
-
-class OrdinalRepository(AbstractRepository[Ordinal]):
-    class Meta:
-        collection_name = "ordinals"
-
-
-    # def __init__(self):
-    #     self.db = get_db()
-    #     self.collection = self.db["ordinals"]
-
-    # def get_ordinals(self):
-    #     ordinals = self.collection.find()
-    #     return ordinals
-
-    # def get_ordinal_by_id(self, id):
-    #     query = {"id": id}
-    #     ordinal = self.collection.find(query)
-    #     return ordinal
-
-    # def get_random_ordinal(self):
-    #     count = self.collection.count_documents({})
-    #     if count == 0:
-    #         return None
-
-    #     document = self.collection.find_one()
-    #     if document:
-    #         ordinal = Ordinal(**document)
-    #         return ordinal
-    #     else:
-    #         return None
-
-    # def insert_ordinal(self, ordinal: Ordinal):
-    #     ordinal_dict = ordinal.to_dict()
-    #     result = self.collection.insert_one(ordinal_dict)
-    #     print(f"Inserted ordinal with id: {result.inserted_id}")
-    #     return result.inserted_id
-
-    # def seed_ordinals(self):
-    #     ordinals = load_seed_ordinals()
-
-    #     for ordinal in ordinals:
-    #         self.insert_ordinal(ordinal)
-
-    #     return ordinals
-
 
 def get_db():
     url = os.getenv('MONGO_URL')
@@ -104,18 +55,8 @@ def get_ordinals():
     return ordinals
 
 
-def get_ordinals_2():
-    url = os.getenv('MONGO_URL')
-    client = pymongo.MongoClient(url)
-    database = client["ordinals"]
-    repo = OrdinalRepository(database=database)
-    ordinals = repo.find({"id": "65d7f8fcb0d3e6dc93f9a771"})
-    return ordinals
-
-
 def get_ordinal_by_id():
-    db = get_db()
-    collection = db["ordinals"]
+    collection = get_ordinals_collection()
 
     query = {"id": id}
     ordinal = collection.find(query)
@@ -123,20 +64,16 @@ def get_ordinal_by_id():
 
 
 def get_random_ordinal() -> Ordinal:
-    db = get_db()
-    collection = db["ordinals"]
+    collection = get_ordinals_collection()
     count = collection.count_documents({})
     if count == 0:
         return None
 
-    # random_skip = random.randint(0, count - 1) if count else 0
+    random_skip = random.randint(0, count - 1) if count else 0
+    cursor = collection.find().skip(random_skip).limit(1)
 
-    # ordinal = collection.find().skip(random_skip).limit(1)
-    document = collection.find_one()
-    # print(f"Random ordinal: {ordinal}")
-    # print(type(ordinal))
-
-    if document:
+    if cursor:
+        document = next(cursor)
         ordinal = Ordinal(**document)
         return ordinal
 
@@ -144,10 +81,8 @@ def get_random_ordinal() -> Ordinal:
         return None
 
 
-def insert_ordinal(ordinal: Ordinal):
-    db = get_db()
-    collection = db["ordinals"]
-
+def insert_ordinal(ordinal: Ordinal) -> str:
+    collection = get_ordinals_collection()
     ordinal_dict = ordinal.to_dict()
 
     result = collection.insert_one(ordinal_dict)
--- backend/src/test.py
+++ backend/src/test.py
@@ -10,10 +10,3 @@ ordinals = db.get_ordinals()
 ordinal = collaboration.get_random_ordinal()
 print(ordinal)
 print(type(ordinal))
-
-# ordinals_cursor = db.get_ordinals()
-# ordinal = next(ordinals_cursor)
-# print(ordinal)
-# ordinal_object = Ordinal(**ordinal)
-
-# print(ordinal_object)
