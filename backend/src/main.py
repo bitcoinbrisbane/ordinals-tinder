c306a66ccd21dac3e9d63ca3e073ec2023820a33
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-03-05 13:33:22 +1000

    chore: add cors middlewere

--- backend/src/main.py
+++ backend/src/main.py
@@ -14,10 +14,24 @@ import ml
 
 from fastapi import FastAPI
 from fastapi.responses import Response
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 app = FastAPI()
 
+origins = [
+    "*",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
