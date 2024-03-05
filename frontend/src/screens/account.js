0be62806059a1646f425e2d5274f3e00ee4aa77e
Emmanuel Mendieta <emmanuel_mendieta@icloud.com>
2024-03-04 19:49:32 -0600

    merging?

--- frontend/src/screens/account.js
+++ frontend/src/screens/account.js
@@ -1,40 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const AddressComponent = () => {
-  const [addressData, setAddressData] = useState(null);
-
-  const API_URL = 'https://cors-anywhere.herokuapp.com/http://128.199.176.152/';
+function Test() {
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(API_URL);
-        setAddressData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, [API_URL]); // Make sure to include API_URL in the dependency array
+    axios.get("http://128.199.176.152/address")
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   return (
     <div>
-      <h1>Address Data</h1>
-      {addressData ? (
-        <ul>
-          {Object.entries(addressData).map(([key, value]) => (
-            <li key={key}>
-              <strong>{key}:</strong> {value}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <h1>Data from API:</h1>
+      {data && <div>{data}</div>}
     </div>
   );
-};
+}
 
-export default AddressComponent;
+export default Test;
